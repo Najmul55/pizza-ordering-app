51e1be7825bc415a46bb753a06bf42404f8d9c13
Najmul Hasan <najmulhasankhokon111@gmail.com>
2022-09-13 22:34:06 +0600

    localhost removed

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export const getServerSideProps = async (context) => {
     admin = true;
   }
 
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get("/api/products");
 
   return {
     props: {
